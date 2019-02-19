029eeae06b2f2ce29c48eab7be7835514771a0bd
Juan Carlos M <Juan Carlos M>
2019-02-19 15:43:58 -0600

    iniciando todo

--- index.js
+++
@@ -1,8 +0,0 @@
-const  express = require('express');
-const app = express();
-
-
-app.get('/', (req,res) =>res.send('Hola mundo!first-app-node '));
-
-app.listen(3000, ()=>{console.log('first-app-node esta en port 3000');}); 
-console.log();
